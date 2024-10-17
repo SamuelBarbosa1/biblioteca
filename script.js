ea865b644d3e80a38673353d4a7491a548dbc8d1
SamuelBarbosa1 <samuelbarbosa2001oliveira@gmail.com>
2024-10-17 16:08:23 -0300

    correção

--- script.js
+++ script.js
@@ -3,19 +3,18 @@ function lerLivro(idLivro, caminhoPdf) {
     let iframe = livro.querySelector('iframe');
 
     if (!iframe) {
-        // Criar o iframe se ele ainda não existe
         iframe = document.createElement('iframe');
         iframe.style.width = '100%';
         iframe.style.border = '1px solid #ccc';
         iframe.src = caminhoPdf;
+        iframe.setAttribute('type', 'application/pdf'); 
+        iframe.setAttribute('allowfullscreen', '');
         livro.appendChild(iframe);
     }
 
-    // Ajustar a visibilidade e redimensionamento
     if (iframe.style.display === 'none' || iframe.style.display === '') {
         iframe.style.display = 'block';
 
-        // Ajustar a altura conforme o dispositivo
         if (window.innerWidth <= 768) {
             iframe.style.height = '90vh'; 
         } else {
